30a3f51008d6f7e9222e47495d65ff7120f05270
owhite01 <owhite.white@googlemail.com>
2016-02-18 18:52:14 +0000

    Added a junit test framework setup to the project

---
+++ src/Tests/GameTest.java
@@ -0,0 +1,14 @@
+package Tests;
+import Classes.Game;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    @Test
+    public void randomTest(){
+        assertEquals(4,4);
+    }
+
+
+}
