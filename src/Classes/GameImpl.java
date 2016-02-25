8153df578981bb52ef5c80704f6c999e8c7a055e
owhite01 <owhite.white@googlemail.com>
2016-02-25 22:22:09 +0000

    Added an intro state

--- src/Classes/GameImpl.java
+++ src/Classes/GameImpl.java
@@ -1,35 +1,35 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameImpl extends GameAbstractImpl {
 
     private Renderer renderer;
     private Board board;
 
+    private int currentState;
+    private List<GameState>  gameStates;
+
     public GameImpl(boolean easy) {
-        super(easy);
+       super(easy);
 
         renderer = new TextRenderer();
         board = new Board(4,12);
+
+        gameStates = new ArrayList<>();
+
+        IntroState introState = new IntroState();
+        introState.init();
+        gameStates.add(introState);
     }
 
     @Override
     public void runGames(){
-        System.out.println("Welcome to Mastermind.");
-        System.out.print("This is a text version of the classic board game Mastermind.\n" +
-                "The computer will think of a secret code.\n" +
-                "The code consists of 4 colored pegs.\n" +
-                "The pegs may be one of six colors: blue, green , orange, purple, red, or yellow.\n" +
-                "A color may appear more than once in the code.\n" +
-                "\n" +
-                "You try to guess what colored pegs are in the code and what order they are in\n" +
-                "After making a guess the result will be displayed.\n" +
-                "A result consists of a black peg for each peg you have exactly correct (color and position) in your guess.\n" +
-                "For each peg in the guess that is the correct color, but is out of position, you get a white peg.\n" +
-                "\n" +
-                "Only the first letter of the color is displayed. B for Blue, R for Red, and so forth.\n" +
-                "When entering guesses you only need to enter the first character of the color as a capital letter.\n" +
-                "\n" +
-                "You have 12 to guess the answer or you lose the game.\n");
+
+        //main game loop will happen here.
+        gameStates.get(currentState).update();
+        gameStates.get(currentState).render();
 
         Blue newBlue = new Blue();
         Peg bluePeg = new Peg(newBlue);
