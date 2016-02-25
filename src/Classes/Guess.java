c4eccab30e12d23b70690e26b539b196f4ea58ce
owhite01 <owhite.white@googlemail.com>
2016-02-25 22:04:04 +0000

    Added stub for guess class

---
+++ src/Classes/Guess.java
@@ -0,0 +1,17 @@
+package Classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guess {
+    public Guess(int pegCount) {
+    }
+
+    void setPeg(Peg peg, int index){
+
+    }
+
+    List<Peg> getPegs() {
+        return new ArrayList<>();
+    }
+}
