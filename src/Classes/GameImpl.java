cb9779272da68384385c7cc9a364a73299fdad76
holyhoppsan <daniel.hall@live.se>
2016-04-03 21:08:27 +0100

    removed test code.

--- src/Classes/GameImpl.java
+++ src/Classes/GameImpl.java
@@ -37,8 +37,6 @@ public class GameImpl extends GameAbstractImpl {
         PlayerInputState playerInputState = new PlayerInputState(inputHandler);
         playerInputState.init();
         gameStates.add(playerInputState);
-
-        System.out.println("hi");
     }
 
     @Override
